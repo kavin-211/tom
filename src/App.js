1c28b4f08ef28d2420eda7ef9df244ef3695c1c4
kavin-211 <kavinprasathvgp@gmail.com>
2025-03-30 22:42:43 +0530

    test6

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import React, { useEffect, useState } from "react";
-import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-import img1 from "./image1.png";
-import img2 from "./image2.png";
-import img3 from "./image3.png";
-import img4 from "./image4.png";
+import React, { useState } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-const images = {
-  one: img1,
-  two: img2,
-  three: img3,
-  four: img4,
-};
-
-const VoiceCommandApp = () => {
-  const [imageSrc, setImageSrc] = useState("");
-  const { transcript, listening, resetTranscript } = useSpeechRecognition();
-
-  useEffect(() => {
-    if (transcript) {
-      const command = transcript.toLowerCase();
-      if (images[command]) {
-        setImageSrc(images[command]);
-      }
+const Dictaphone = () => {
+  const [message, setMessage] = useState('')
+  const commands = [
+    {
+      command: 'I would like to order *',
+      callback: (food) => setMessage(`Your order is for: ${food}`)
+    },
+    {
+      command: 'The weather is :condition today',
+      callback: (condition) => setMessage(`Today, the weather is ${condition}`)
+    },
+    {
+      command: 'My top sports are * and *',
+      callback: (sport1, sport2) => setMessage(`#1: ${sport1}, #2: ${sport2}`)
+    },
+    {
+      command: 'Pass the salt (please)',
+      callback: () => setMessage('My pleasure')
+    },
+    {
+      command: ['Hello', 'Hi'],
+      callback: ({ command }) => setMessage(`Hi there! You said: "${command}"`),
+      matchInterim: true
+    },
+    {
+      command: 'Beijing',
+      callback: (command, spokenPhrase, similarityRatio) => setMessage(`${command} and ${spokenPhrase} are ${similarityRatio * 100}% similar`),
+      // If the spokenPhrase is "Benji", the message would be "Beijing and Benji are 40% similar"
+      isFuzzyMatch: true,
+      fuzzyMatchingThreshold: 0.2
+    },
+    {
+      command: ['eat', 'sleep', 'leave'],
+      callback: (command) => setMessage(`Best matching command: ${command}`),
+      isFuzzyMatch: true,
+      fuzzyMatchingThreshold: 0.2,
+      bestMatchOnly: true
+    },
+    {
+      command: 'clear',
+      callback: ({ resetTranscript }) => resetTranscript()
     }
-  }, [transcript]);
+  ]
 
-  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
-    return <p>Your browser does not support speech recognition.</p>;
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands })
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
   }
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Voice Command App</h1>
-      <button
-        onClick={listening ? SpeechRecognition.stopListening : SpeechRecognition.startListening}
-        style={{ marginBottom: "20px" }}
-      >
-        {listening ? "Stop Listening" : "Start Listening"}
-      </button>
-      <p>Recognized Command: {transcript}</p>
-      {imageSrc && (
-        <img
-          src={imageSrc}
-          alt="Recognized command"
-          style={{ marginTop: "20px", width: "300px", height: "auto" }}
-        />
-      )}
-      <div style={{ marginTop: "20px" }}>
-        {Object.keys(images).map((key) => (
-          <button key={key} onClick={() => setImageSrc(images[key])} style={{ margin: "5px" }}>
-            {key.charAt(0).toUpperCase() + key.slice(1)}
-          </button>
-        ))}
-      </div>
+    <div>
+      <p>{message}</p>
+      <p>{transcript}</p>
     </div>
-  );
-};
-
-export default VoiceCommandApp;
+  )
+}
+export default Dictaphone;
